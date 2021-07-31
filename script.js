ef0aca682a103de1c2b05e6c2a44a258acb3a37b
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-07-31 13:22:55 -0600

    psuedocode java -S

---
+++ script.js
@@ -0,0 +1,19 @@
+//pull classes from html 
+
+//
+// Function to go and auto fill when users collect 
+// $(function () {
+//     //put array to run through- pulldown/autofill
+//     var array = ['soap', 'beer', 'monkies'] - every 
+//     //autocomplete is a completed widget that we just need to call 
+//     //taking given skill and pulling it 
+//     $('#skill-name').autocomplete({
+//       //there has to be a source otherwise it won't work 
+//       source: array
+//     }) 
+//   });
+
+//take options from autofill to run through the apis 
+    //keep choices and search api for the food receipes 
+
+
