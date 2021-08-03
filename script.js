9b5ac86651abe6fbe81388ad70d4997e36639009
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-08-03 12:19:13 -0600

    added drop down and added some IDs and divs to html

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ $(document).ready(function () {
     const searchEl = document.querySelectorAll(".btn");
     const recipeEl = document.getElementById("recipe-area");
 
-//Function to go and auto fill when users collect 
-$(function autoFillFood () {
-    //put array to run through- pulldown/autofill
-    userChoices.toLowerCase(); //make sure that it can be read no matter what 
+//Function to go and auto fill/drop down  when users collect 
+$(function () {
+
     //wonder if we can't use the API for the drop down and could completely ignore this?? 
     var autoFillList = ['apple', 'avocado', 'apricots', 'lemons', 'pears', 'banana', 'melon', 'fruits', 'baby spinach', 'mixed greens', 'broccoli',
     'cauliflower', 'carrots', 'herbs', 'potatoes','onions', 'garlic', 'peppers', 'roots', 'salmon', 'cod', 'shrimp', 'tortillas', 'bread', 
-    'pitas', 'bacon', 'sausage', 'beef', 'pork', 'chicken', 'ham', 'turkey', 'canned tomatos', 'black beans', 'pinto beans', 'milk', 'butter', ''] 
+    'pitas', 'bacon', 'sausage', 'beef', 'pork', 'chicken', 'ham', 'turkey', 'canned tomatos', 'black beans', 'pinto beans', 'milk', 'butter']; 
     //autocomplete is a completed widget that we just need to call 
     //taking given skill and pulling it 
+    
     $('#food-list').autocomplete({
       source: autoFillList
     }) 
