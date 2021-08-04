a5ff6bb5b408d115559a1fc4f766444f6f2b6dc4
Amanda McBee <ahanes35@yahoo.com>
2021-08-03 19:47:16 -0600

    the fridge is running -AM

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,73 +7,84 @@ $(document).ready(function () {
     const searchEl = document.querySelectorAll(".btn");
     const recipeEl = document.getElementById("recipe-area");
     let userListFood = document.getElementById("userFridge");
+});
 
-//creates list items 
-    var addElement = function (food) {
-      // makes list to then be appended to empty div in index.html
-      var listEl = $('<li>');
-      //appending part
-      listEl.appendTo(userListFood);
-    
-  //on click takes food list to go onto DOM 
-      let foodPicked = function (e) {
-     e.preventDefault();
-    
-      var foodChoice = userChoices.val();
-    
-      if (!foodChoice) {
-        console.log('You need to fill out the form!');
-        return;
-      }
-    
-      addElement(foodChoice, food);
-    
-      userChoices.val('');
-    $("#addBtn").on('submit', addElement);
-}};
- 
-    });
-//Function to go and auto fill/drop down  when users collect 
-$(function foodList () {
+var foodInput = document.querySelector("#food-list");
+var formEl = document.querySelector("#food-form");
+var fridge = document.querySelector("#userFridge");
+// var todoCountSpan = document.querySelector("#todo-count");
+var items = [];
 
-    //wonder if we can't use the API for the drop down and could completely ignore this?? 
-    const autoFillList = ['apple', 'avocado', 'apricots', 'lemons', 'pears', 'banana', 'melon', 'fruits', 'baby spinach', 'mixed greens', 'broccoli',
-    'cauliflower', 'carrots', 'herbs', 'potatoes','onions', 'garlic', 'peppers', 'roots', 'salmon', 'cod', 'shrimp', 'tortillas', 'bread', 
-    'pitas', 'bacon', 'sausage', 'beef', 'pork', 'chicken', 'ham', 'turkey', 'canned tomatos', 'black beans', 'pinto beans', 'milk', 'butter']; 
-
-    $('#food-list').autocomplete({
-      source: autoFillList
-    }) 
+// TODO: What is the purpose of this function? .....................will pull the todos
+function renderItems() {
+  // TODO: Describe the functionality of the following two lines of code. ........pulling from innerHTML to DOM including length for loop
+  fridge.innerHTML = ""; 
+  //item.CountSpan.textContent = todos.length;/*updates todo count*/
+  
+  // TODO: Describe the functionality of the following `for` loop. takes in user input and makes it into an element and creates the list item and the complete button and pushes onto dom
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item);
+// creates list item and pushes to the dom
+    var li = document.createElement("li");
+    li.textContent = item;
+    li.setAttribute("data-index", i);
+//creates element to be pushed onto the dom
+    var button = document.createElement("button");
+    button.textContent = "Remove";
+//appends to go onto dom once created
+    li.appendChild(button);
+    fridge.appendChild(li);
+    //when creating element- remember to append or will not show up on dom
   }
-  //function here to save user choices to then run through next function 
-
-  );
-
-
-
-function getRecipe() {
-// where fetch will be
-const APIKeyRecipe = '3aa6e2558540ee0b95bb5b427b5c3a98	—'
-var APIsite = ''
-//where to put empty array of food? 
-// from rapid api thingy (idk if it will work)
-// fetch("https://edamam-recipe-search.p.rapidapi.com/search?q=chicken", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
-
 }
 
-//take options from autofill to run through the apis 
-    //keep choices and search api for the food receipes 
+// TODO: What is the purpose of the following function?
+function init() {
+  // TODO: What is the purpose of the following line of code?
+  //takes a string  oand turns it into an object
+  var storedItems = JSON.parse(localStorage.getItem("items"));
+  // TODO: Describe the functionality of the following `if` statement.          makes it so if nothing is in the string nothing will be added
+  if (storedItems !== null) {
+    items = storedItems;
+  }
+  // TODO: Describe the purpose of the following line of code.     runs the function, pulls info back
+  renderItems();
+}
 
+function storeItems() {
+  // TODO: Describe the purpose of the following line of code.      turns object into a string
+  localStorage.setItem("items", JSON.stringify(items));
+}
 
-// only one recipe- if don't like it, run through function again
+// TODO: Describe the purpose of the following line of code.  when event submit is clicked it takes it to local storage
+formEl.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var itemText = foodInput.value.trim();
+  // TODO: Describe the functionality of the following `if` statement. if text box is left blank, return a 
+  if (itemText === "") {
+    return;
+  }
+ // TODO: Describe the purpose of the following lines of code.       adding a new value to that array 
+  items.push(itemText);
+  foodInput.value = "";
+ 
+  // TODO: What will happen when the following functions are called? .......will take the inputs and store them ...........recalls the info
+  storeItems();
+  renderItems();
+});
+
+// TODO: Describe the purpose of the following line of code. ....... when you click this button, 
+fridge.addEventListener("click", function(event) {
+  var element = event.target;
+  // TODO: Describe the functionality of the following `if` statement.
+  if (element.matches("button") === true) {
+    var index = element.parentElement.getAttribute("data-index");
+    items.splice(index, 1);
+    // TODO: What will happen when the following functions are called?
+    storeItems();
+    renderItems();
+  }
+});
+
+init();
