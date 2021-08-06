75e548786f589822b1c45cfa91f33fa900a4ea03
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-08-06 15:16:30 -0600

    General clean up of code -S

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,57 +15,44 @@ var peanutFree = document.querySelector('input[id="peanutFree"]');
 
 // Will pull the food items from the fridge
 function renderItems() {
-  // Pulling from innerHTML to DOM including length for loop
   fridge.innerHTML = ""; 
-// Takes in user input and makes it into an element and creates the list item and the complete button and pushes onto dom
   for (var i = 0; i < items.length; i++) {
     var item = items[i];
     console.log(item);
-    // creates list item and pushes to the dom
     var ul = document.createElement("ul");
     ul.textContent = item;
     ul.setAttribute("data-index", i);
-    //creates element to be pushed onto the dom
     var button = document.createElement("button");
     button.textContent = "Remove";
-    //appends to go onto dom once created
     ul.appendChild(button);
     fridge.appendChild(ul);
   }
 }
-
 // Get stored items from local storage
 function init() {
-  //takes a string  oand turns it into an object
   var storedItems = JSON.parse(localStorage.getItem("items"));
-  // if items were retrieved from local storage update the stored items to it
   if (storedItems !== null) {
     items = storedItems;
   }
-  // helper function that will render items to the DOM
   renderItems();
 }
-
+//stores item 
 function storeItems() {
-  // turns object into a string and set key in local storage
   localStorage.setItem("items", JSON.stringify(items));
 }
 
-// when you add food to the input area, it creates a variable fot it which will put it in the fridge
-formEl.addEventListener("submit", function(event) {
+//creates variable 
+var saveAndPush = function(event) {
   event.preventDefault();
   var itemText = foodInput.value.trim(); 
   if (itemText === "") {
     return;
   }
- // adding a new value to that array 
   items.push(itemText);
   foodInput.value = "";
- 
-  // will take the inputs and store them => recalls the info
   storeItems();
   renderItems();
-});
+};
 
 // when you click this button, 
 fridge.addEventListener("click", function(event) {
@@ -80,37 +67,24 @@ fridge.addEventListener("click", function(event) {
   }
 });
 
-init();
 
-
-
-// start API 
-//add event listen - on search btn 
-
-searchEl.addEventListener("click", function() {
+// Gets recipe based on userInput 
+var getFoodApi = function() {
     var storedItems = JSON.parse(localStorage.getItem("items"));
   var foodAPI = 'https://api.edamam.com/api/recipes/v2?type=public&q=' + storedItems + '&app_id=0bef8d90&app_key=3aa6e2558540ee0b95bb5b427b5c3a98' + allergyChoices
-  //  + '&health=' + allergies;
+  //  per_page=5
   fetch(foodAPI) 
     .then(response => response.json()) 
     .then(data => {
       console.log(data);
+
       var recipe = document.createElement('li'); 
-      recipe.textContent = data.hits[recipe];
+      recipe.textContent = data.hits[0];
       recipeEl.appendChild(recipe);
     })
-    
-  })
+  }
 
-//on click, box checked add event.target.value to array
-
-
-
-if(dairyFree.checked = true) {
-  console.log("yay");
-} else {
-  console.log("WRONG");
-}
-//per_page=5
-
+formEl.addEventListener("submit", saveAndPush)
+searchBtn.addEventListener("click", getFoodApi);
+init();
 
