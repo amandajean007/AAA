38f67cd90ec59399e0f53db2f5c85f56a1256882
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-08-08 15:36:27 -0600

    switched ajax to fetch to be more consistant -S

--- assets/js/adventure.js
+++ assets/js/adventure.js
@@ -1,181 +1,86 @@
 var apiKey = "9973533";
-
 var randomDrinkURL = `https://www.thecocktaildb.com/api/json/v2/${apiKey}/random.php`;
 var randomFoodURL = `https://www.themealdb.com/api/json/v2/${apiKey}/random.php`;
 
+var randomize = document.querySelector('#randomizer');
+var randomDrink = document.querySelector('#randomDrink');
+var randomFood = document.querySelector('#randomFood')
 
-$("#randomizer").on("click", function() {
-    $.ajax({
-        url: randomDrinkURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        $("#randomDrink").empty();
+randomize.addEventListener("click", function () {
+    fetch(randomDrinkURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            showDrink(data);
+            console.log(data);
+        });
 
-        var drinkCard = $("<div>");
-        drinkCard.addClass("card card-resize");
-        drinkCard.attr("data-drink-id");
-        
-        var drinkThumbDiv = $("<div>");
-        drinkThumbDiv.addClass("card-image waves-effect waves-block waves-light")
-        var drinkThumb = $("<img>");
-        drinkThumb.addClass("activator");
-        drinkThumb.attr("src", response.drinks[0].strDrinkThumb);
+    function showDrink(data) {
+        //beginning div
+        var drinkCard = document.createElement("div");
+        drinkCard.classList = "card card-resize"
+
+        // img div
+        var drinkThumbDiv = document.createElement("div");
+        drinkThumbDiv.classList = "card-image waves-effect waves-block waves-light"
+        // the image
+        var drinkThumb = document.createElement("img");
+        drinkThumb.classList = "activator"
+        drinkThumb.setAttribute("src", data.drinks[0].strDrinkThumb);
         drinkThumbDiv.append(drinkThumb);
-        
-        var drinkNameDiv = $("<div>");
-        drinkNameDiv.addClass("card-content");
-        var drinkName = $("<span>");
-        drinkName.addClass("card-title activator grey-text text-darken-4");
-        drinkName.text(response.drinks[0].strDrink);                
-        var recipeButton = $("<i>");
-        recipeButton.addClass("material-icons right");
-        recipeButton.text("more_vert");
-        drinkName.append(recipeButton);
+        // drink name div
+        var drinkNameDiv = document.createElement("div");
+        drinkNameDiv.classList = "card-content"
+        //drink name 
+        var drinkName = document.createElement("span");
+        drinkName.classList = "card-title activator grey-text text-darken-4"
+        //drinkName.href = 
+        drinkName.textContent = data.drinks[0].strDrink
+        //all the appends
         drinkNameDiv.append(drinkName);
-        
-        var recipeReveal = $("<div>");
-        recipeReveal.addClass("card-reveal");
-        var recipeInfo = $("<span>");
-        recipeInfo.addClass("card-title activator grey-text text-darken-4");
-        recipeInfo.text(response.drinks[0].strDrink)
-        var closeRecipe = $("<i>");
-        closeRecipe.addClass("material-icons right");
-        closeRecipe.text("close");
-        var recipeDetails = $("<div>");
-        recipeDetails.addClass("recipe-details");
-        recipeDetails.attr("id", response.drinks[0].idDrink);
-        recipeInfo.append(closeRecipe);
-        recipeReveal.append(recipeInfo);
-        recipeReveal.append(recipeDetails);
-
         drinkCard.append(drinkThumbDiv);
         drinkCard.append(drinkNameDiv);
-        drinkCard.append(recipeReveal);
-        $("#randomDrink").append(drinkCard);
-                    
-        var abv = $("<p>");
-        abv.text(response.drinks[0].strAlcoholic);
-        recipeReveal.append(abv);
-        abv.addClass("abv");
+        randomDrink.append(drinkCard);
+    }
 
-        var measurementDiv = $("<div>");
-        measurementDiv.addClass("recipe-measurements")
-        recipeReveal.append(measurementDiv);
+    fetch(randomFoodURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            showFood(data);
+            console.log(data)
+        });
 
-        var ingredientDiv = $("<div>");
-        ingredientDiv.addClass("recipe-ingredients")
-        recipeReveal.append(ingredientDiv);
-        
-        for (var i = 1; i < 16; i++) {
-            var objKeyMeasure = `strMeasure${i}`;
-            
-            if (response.drinks[0][objKeyMeasure]) {
-                // console.log(response.drinks[0][objKeyMeasure]);
-                var recipeMeasureDisplay = $("<p>");
-                recipeMeasureDisplay.text(response.drinks[0][objKeyMeasure]);
-                measurementDiv.append(recipeMeasureDisplay);
-            }
-        }
+    function showFood(data) {
+        //beginning div
+        var foodCard = document.createElement("div");
+        foodCard.classList = "card card-resize"
 
-        for (var i = 1; i < 16; i++) {
-            var objKeyIngredient = `strIngredient${i}`;
-            
-            if (response.drinks[0][objKeyIngredient]) {
-                // console.log(response.drinks[0][objKeyIngredient]);
-                var recipeIngredientDisplay = $("<p>");
-                recipeIngredientDisplay.text(response.drinks[0][objKeyIngredient]);
-                ingredientDiv.append(recipeIngredientDisplay);
-            }
-        }
-
-        var recipeInstructions = $("<p>");
-        recipeInstructions.text(response.drinks[0].strInstructions);
-        recipeInstructions.addClass("drinkRecipeInstructions");
-        recipeReveal.append(recipeInstructions);
-    });
-
-
-    $.ajax({
-        url: randomFoodURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        $("#randomFood").empty();
-
-        var foodCard = $("<div>");
-        foodCard.addClass("card card-resize");
-        foodCard.attr("data-drink-id", );
-        
-        var foodThumbDiv = $("<div>");
-        foodThumbDiv.addClass("card-image waves-effect waves-block waves-light")
-        var foodThumb = $("<img>");
-        foodThumb.addClass("activator");
-        foodThumb.attr("src", response.meals[0].strMealThumb);
+        //food img div 
+        var foodThumbDiv = document.createElement("div");
+        foodThumbDiv.classList = "card-image waves-effect waves-block waves-light"
+        // food img
+        var foodThumb = document.createElement("img");
+        foodThumb.classList = "activator"
+        foodThumb.setAttribute("src", data.meals[0].strMealThumb);
         foodThumbDiv.append(foodThumb);
-        
-        var foodNameDiv = $("<div>");
-        foodNameDiv.addClass("card-content");
-        var foodName = $("<span>");
-        foodName.addClass("card-title activator grey-text text-darken-4");
-        foodName.text(response.meals[0].strMeal);                
-        var foodRecipeButton = $("<i>");
-        foodRecipeButton.addClass("material-icons right");
-        foodRecipeButton.text("more_vert");
-        foodName.append(foodRecipeButton);
-        foodNameDiv.append(foodName);
-        
-        var foodRecipeReveal = $("<div>");
-        foodRecipeReveal.addClass("card-reveal");
-        var foodRecipeInfo = $("<span>");
-        foodRecipeInfo.addClass("card-title activator grey-text text-darken-4");
-        foodRecipeInfo.text(response.meals[0].strMeal)
-        var closeFoodRecipe = $("<i>");
-        closeFoodRecipe.addClass("material-icons right");
-        closeFoodRecipe.text("close");
-        var foodRecipeDetails = $("<div>");
-        foodRecipeDetails.addClass("recipe-details");
-        foodRecipeDetails.attr("id", response.meals[0].idMeal);
-        foodRecipeInfo.append(closeFoodRecipe);
-        foodRecipeReveal.append(foodRecipeInfo);
-        foodRecipeReveal.append(foodRecipeDetails);
 
+        //food name div
+        var foodNameDiv = document.createElement("div");
+        foodNameDiv.classList = "card-content"
+        //food name 
+        var foodName = document.createElement("a");
+        foodName.classList = "card-title activator grey-text text-darken-4"
+        foodName.textContent = data.meals[0].strMeal
+        foodName.href = data.meals[0].strSource
+        foodNameDiv.append(foodName);
+
+        //all the appends 
         foodCard.append(foodThumbDiv);
         foodCard.append(foodNameDiv);
-        foodCard.append(foodRecipeReveal);
-        $("#randomFood").append(foodCard);
+        randomFood.append(foodCard);
 
-        var foodMeasurementDiv = $("<div>");
-        foodMeasurementDiv.addClass("recipe-measurements")
-        foodRecipeReveal.append(foodMeasurementDiv);
-
-        var foodIngredientReveal = $("<div>");
-        foodIngredientReveal.addClass("recipe-ingredients")
-        foodRecipeReveal.append(foodIngredientReveal);
-        
-        for (var i = 1; i < 21; i++) {
-            var objKeyMeasure = `strMeasure${i}`;
-            
-            if (response.meals[0][objKeyMeasure]) {
-                var recipeMeasureDisplay = $("<p>");
-                recipeMeasureDisplay.text(response.meals[0][objKeyMeasure]);
-                foodMeasurementDiv.append(recipeMeasureDisplay);
-            }
-        }
-
-        for (var i = 1; i < 21; i++) {
-            var objKeyIngredient = `strIngredient${i}`;
-            
-            if (response.meals[0][objKeyIngredient]) {
-                var recipeIngredientDisplay = $("<p>");
-                recipeIngredientDisplay.text(response.meals[0][objKeyIngredient]);
-                foodIngredientReveal.append(recipeIngredientDisplay);
-            }
-        }
-
-        var recipeInstructions = $("<p>");
-        recipeInstructions.text(response.meals[0].strInstructions);
-        recipeInstructions.addClass("drinkRecipeInstructions");
-        foodRecipeReveal.append(recipeInstructions);
-    });
+    }
 });
